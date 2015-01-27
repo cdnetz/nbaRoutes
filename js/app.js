8af492d5e909d8c8de82ea253cdbd4d192b09442
Chris Netz <cnetz89@gmail.com>
2015-01-26 20:20:23 -0700

    step 5, can't get anything to show

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@ var app = angular.module('nbaRoutes', ['ngRoute']);
 
 app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
+  $routeProvider
+  .when('/', {
+  	templateUrl: 'js/home/homeTmpl.html',
+  	controller: 'homeCtrl'
+  }).when ('/teams/:team', {
+  	templateUrl: 'js/teams/teamTmpl.html',
+  	controller: 'teamCtrl',
+  	resolve: {
+  		teamData: function(teamService, $route){
+  			teamService.getTeamData($route.current.params.team).then(
+  				function(results) {
+  					return results;
+  				})
+  				
+  		}
+  	}
+  
+})
+  .otherwise ({
+  	redirectTo: '/'
+  })
 
-  //router here
+  
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,59 @@
 var app = angular.module('nbaRoutes');
 
-app.service('teamService', function($http, $q){
+app.service('teamService', function($http, $q, teamData){
+	this.getTeamData = function(team){
+		$q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
 
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++) {
+				if (results[i].won) {
+					wins += 1;
+
+				};
+				if (!resluts[i].won) {
+					losses -= 1;
+				};
+			};
+			results.wins = wins;
+			results.losses = losses;
+
+			defered.resolve(results)
+		}, function(err){
+			defered.reject(err);
+		})
+
+		
+
+		
+
+		return defered.promise;
+
+
+	}
+
+	this.addNewGame = function(gameObject) {
+	var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+
+
+	if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+		gameObj.won = true;
+	} else {
+		gameObj.won = false;
+	}
+	$http({
+		method: 'POST',
+		url: url,
+		data: gameObj
+	})
+
+	
+}
 });
+
